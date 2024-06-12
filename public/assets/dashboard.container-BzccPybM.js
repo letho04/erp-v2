7a898054b91b0e42c194cc232b6f978cca9e5519
Tran.Minh.Phat <phatminh900@gmail.com>
2024-06-12 09:35:18 +0700

    adjust some minor changes

--- public/assets/dashboard.container-BzccPybM.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ihE4iNTm.js";const a=()=>r.jsx("p",{children:"Dashboard"});export{a as default};
---
+++ public/assets/dashboard.container-DKK74NKV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DgYPZxAJ.js";const a=()=>r.jsx("p",{children:"Dashboard"});export{a as default};
